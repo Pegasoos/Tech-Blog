2129b5ea7e17f6f856a58167c39bd552f134e462
Pegasoos <circumambulation24@yahoo.com>
2021-11-28 19:39:20 -0500

    Server.js static server functional

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const path = require("path");
-
+const exphbs = require("express-handlebars");
 const app = express();
 
 const sequelize = require("./config/connection");
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () =>{console.log(`Serving at http://localhost:${PORT}`)})
