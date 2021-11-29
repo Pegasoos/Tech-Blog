80f56079873b427026b8f37c7275a5751ec72b4e
Pegasoos <circumambulation24@yahoo.com>
2021-11-29 08:35:03 -0500

    Main.handlebars basic layout

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const app = express();
-
+//const routes = require(./controllers)
 const sequelize = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(routes);
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () =>{console.log(`Serving at http://localhost:${PORT}`)})
