974a71cba1f653c16d42b9d94293558359ad11b7
Pegasoos <circumambulation24@yahoo.com>
2021-12-04 00:08:50 -0500

    Added router paths to server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const app = express();
 require('dotenv').config();
 
-//const routes = require(./controllers)
+const routes = require('./controllers');
 const sequelize = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(routes);
+app.use(routes);
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () =>{console.log(`Serving at http://localhost:${PORT}`)})
