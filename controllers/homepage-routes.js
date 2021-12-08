b8eb0da972a953c723722db47ba925fdfed9dd94
Pegasoos <circumambulation24@yahoo.com>
2021-12-08 03:42:00 -0500

    Post route added

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User, Post } = require('../models');
 //add auth require later
 
+//route to render homepage and blog posts
 router.get('/', async (req, res) =>{
     try{
         const allPostData = await Post.findAll();
@@ -13,7 +14,20 @@ router.get('/', async (req, res) =>{
         res.status(500).json(err);
     }
 });
+
+router.get('/post/:id', async (req,res) =>{
+    try{
+    const postData = await Post.findByPk(req.params.id);
+    const post = postData.get({ plain:true });
+    res.render('post', {post});
+}
+catch(err){
+    console.log(err);
+    res.status(500).json(err);
+}
+});
+
 router.get('/login', async (req, res) =>{
-    res.render('login')
-})
+    res.render('login')//add log in condition later
+});
 module.exports = router;
