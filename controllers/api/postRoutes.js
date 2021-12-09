49f2dc43a02b92a6eb68680be03c9c0246e2ddcc
Pegasoos <circumambulation24@yahoo.com>
2021-12-08 19:20:09 -0500

    Post post route functional

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
 const Post = require('../../models/Post');
+
+router.post('/', async (req, res) =>{
+    try{
+        const newPost = await Post.create({
+            ...req.body,
+        });
+        res.status(200).json(newPost);
+    }
+    catch(err){
+        res.status(400).json(err);
+    }
+})
+
 router.delete('/:id', async (req, res) =>{
     try{
         const postData = await Post.destroy({
