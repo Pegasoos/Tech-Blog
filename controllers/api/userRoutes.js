56f5e35b941e26b77f5fb98dcaee9f1b1133af8a
Pegasoos <circumambulation24@yahoo.com>
2021-12-08 20:44:27 -0500

    User Post Route Added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
-
+const User = require('../../models/User');
+//route for creating a new user
+router.post('/', async (req, res) =>{
+try{
+const newUser = await User.create(req.body)
+res.status(200).json(newUser);
+}
+catch (err){
+    res.status(400).json(err);
+}
+});
 module.exports = router;
