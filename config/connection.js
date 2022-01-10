4824f63c43b6e9e1dc75966ba322ec92d4824a38
Pegasoos <circumambulation24@yahoo.com>
2022-01-09 19:47:57 -0500

    Added alternate heroku database connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
-
-let sequelize = new Sequelize(
+let sequelize;
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -11,5 +13,5 @@ let sequelize = new Sequelize(
         dialect:'mysql',
         port:3306
     });
-
-    module.exports = sequelize;
+};
+module.exports = sequelize;
