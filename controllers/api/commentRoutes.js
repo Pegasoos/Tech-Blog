28a3624a4591309d0d95f07bc6f33419bb44060c
Pegasoos <circumambulation24@yahoo.com>
2022-05-31 03:20:36 -0400

    logout.js .fetch route fixed

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const Comment = require('../../models/Comment');
-const withAuth = require('../../utils/auth');
 
 router.post('/', async (req,res) =>{
     try{
